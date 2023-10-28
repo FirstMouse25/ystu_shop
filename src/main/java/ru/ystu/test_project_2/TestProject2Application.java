b57356df65725c880149d6c92c323923b57f5ddc
FirstMouse25 <andrey-tarasov-25@yandex.ru>
2023-10-28 14:55:41 +0300

    create_project

---
+++ src/main/java/ru/ystu/test_project_2/TestProject2Application.java
@@ -0,0 +1,13 @@
+package ru.ystu.test_project_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProject2Application.class, args);
+	}
+
+}
